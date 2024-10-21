c6f17650dcabbdd7b2880ec8477ee803a1a92cdd
Callum Hill <hillcallum@Callums-MacBook-Pro.local>
2024-10-21 15:34:59 +0100

    Fixing the Previous Error

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def receive():
-    return reender_template("index.html")
+    return render_template("index.html")
 
 
 @app.route("/submit", methods=["POST"])
