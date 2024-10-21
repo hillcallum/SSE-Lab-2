d2d405ab4d31cf40f2c742da314ac185b37246d6
Miriam Tiefel <mst124@ic.ac.uk>
2024-10-21 16:41:04 +0100

    reformatted

--- app.py
+++ app.py
@@ -2,20 +2,23 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def receive():
     return render_template("index.html")
+
 
 @app.route("/submit", methods=["POST"])
 def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
     input_color = request.form.get("color")
-    
-    query = input_name  
+
+    query = input_name
     result = process_query(query)
-    
+
     return render_template("result.html", result=result)
+
 
 def process_query(query):
     if query.lower() == "dinosaurs":
