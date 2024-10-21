c93a1f94e8c52687b6f02b7498fe80a593b887de
Callum Hill <hillcallum@Callums-MacBook-Pro.local>
2024-10-21 14:13:53 +0100

    Fixes

--- app.py
+++ app.py
@@ -2,19 +2,21 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def receive():
-    return render_template("index.html")  
+    return render_template("index.html")
+
 
 @app.route("/submit", methods=["POST"])
 def submit():
-    input_name = request.form.get("name")  
-    input_age = request.form.get("age")    
-    input_color = request.form.get("color")  
-    
-    result = f"Hello {input_name}! You are {input_age} years old and your favourite colour is {input_color}."
-    
-    return render_template("result.html", result=result)  # Render results page
+    input_name = request.form.get("name")
+    input_age = request.form.get("age")
+    input_color = request.form.get("color")
 
-if __name__ == "__main__":
-    app.run(debug=True)  # Run the app in debug mode
+    result = (
+        f"Hello {input_name}! You are {input_age} years old "
+        f"and your favourite colour is {input_color}."
+    )
+
+    return render_template("result.html", result=result)
